3236c446b71b6fc1cf62986548caae3445073d1f
rohobotya <yaikobrohobot@gmail.com>
2025-08-04 19:26:29 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC_B_Nb_J2oNyueeBE3OnJKmY8rS1ZxS3U",
+  authDomain: "zowiweb.firebaseapp.com",
+  projectId: "zowiweb",
+  storageBucket: "zowiweb.firebasestorage.app",
+  messagingSenderId: "38401403638",
+  appId: "1:38401403638:web:cbf441adf3c71f922c9ad7",
+  measurementId: "G-TGMF36PEGX"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
